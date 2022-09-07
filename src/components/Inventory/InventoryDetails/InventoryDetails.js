60c0596ec8b769bbda4c33b34e865e023a35cdc2
Graziela Pauselius <grazielak.pauselius@gmail.com>
2022-09-07 15:38:33 -0700

    Create Inventory Details component

--- src/components/Inventory/InventoryDetails/InventoryDetails.js
+++ src/components/Inventory/InventoryDetails/InventoryDetails.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function InventoryDetails() {
-  return (
-    <div>InventoryDetails</div>
-  )
-}
+import ArrowIcon from "../../../assets/icons/arrow_back-24px.svg";
+import EditIcon from "../../../assets/icons/edit-24px.svg";
 
-export default InventoryDetails
+const API_URL = process.env.REACT_APP_API_URL;
+
+const InventoryDetails = () => {
+	const [item, setItem] = useState(null);
+
+	const { itemId } = useParams();
+	//------- Fetch item details data ------
+	useEffect(() => {
+		const getItemDetails = async () => {
+			try {
+				const response = await axios.get(`${API_URL}/id`);
+				console.log(response);
+			} catch (error) {
+				console.log(error.message);
+			}
+		};
+	});
+	return (
+		<section className="inventory-item">
+			<div className="inventory-item__header">
+				<img src={ArrowIcon} alt="" />
+				<h1>{}</h1>
+				<img src={EditIcon} alt="" />
+			</div>
+			<div className="inventory-item__details">
+				<p className="inventory-item__label">ITEM DESCRIPTION</p>
+				<p className="invenroty-item__content">{}</p>
+				<p className="inventory-item__label">CATEGORY</p>
+				<p className="invenroty-item__content">{}</p>
+
+				<p className="inventory-item__label">STATUS</p>
+				<p className="invenroty-item__content">{}</p>
+
+				<p className="inventory-item__label">QUANTITY</p>
+				<p className="invenroty-item__content">{}</p>
+
+				<p className="inventory-item__label">WAREHOUSE</p>
+				<p className="invenroty-item__content">{}</p>
+			</div>
+		</section>
+	);
+};
+
+export default InventoryDetails;
--- src/pages/InventoryPage.js
+++ src/pages/InventoryPage.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import InventoryDetails from "../components/Inventory/InventoryDetails/InventoryDetails";
 
 function InventoryPage() {
-  return (
-    <div>InventoryPage</div>
-  )
+	return <InventoryDetails />;
 }
 
-export default InventoryPage
+export default InventoryPage;
