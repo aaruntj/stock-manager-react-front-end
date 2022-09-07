88f0f22ca49d8ebf8c06a830bbbae3fb7044c8bb
Michael Mifsud-Sweeney <michaelmifsudsweeney@gmail.com>
2022-09-07 19:57:46 -0400

    add tablet styling

--- src/components/Inventory/AddInventory/AddInventory.js
+++ src/components/Inventory/AddInventory/AddInventory.js
@@ -37,11 +37,11 @@ function AddInventory() {
               <label htmlFor="itemStatus" className='item-availability__status-label'>Status</label>
               <div className="item-availability__radio-buttons">
                 <div className="item-availability-radio-button">
-                  <input type="radio" id="item-status__in-stock" name="item-status" value="in stock" />
+                  <input type="radio" className="item-status__in-stock" name="item-status" value="in stock" checked />
                   <label htmlFor="item-status">In stock</label>
                 </div>
                 <div className="item-availability__radio-button">
-                  <input type="radio" id="item-status__out-stock" name="item-status" value="Out of stock" />
+                  <input type="radio" className="item-status__out-stock" name="item-status" value="Out of stock" />
                   <label htmlFor="item-status">Out of stock</label>
                 </div>
               </div>
@@ -49,7 +49,7 @@ function AddInventory() {
               <input type="text" name="quantity" className="item-availability__quantity-input" placeholder='0' />
               <label htmlFor="itemWarehouse" className='item-availability__warehouse-label'>Warehouse</label>
               <select name="itemWarehouse" className="item-availability__item-warehouse-input">
-                <option value="" selected hidden>Please select</option>
+                <option value="" defaultValue hidden>Please select</option>
                 <option value="volvo">Volvo</option>
                 <option value="saab">Saab</option>
                 <option value="mercedes">Mercedes</option>
