9a3a13d6511f7c5b91e2b308329bbacd62ae691e
Michael Mifsud-Sweeney <michaelmifsudsweeney@gmail.com>
2022-09-07 22:58:52 -0400

    add html, css, and props for component

--- src/components/Warehouse/DetailsWarehouse/WarehouseDetails.js
+++ src/components/Warehouse/DetailsWarehouse/WarehouseDetails.js
@@ -1,8 +1,59 @@
 import React from 'react'
+import './WarehouseDetails.scss'
 
-function WarehouseDetails() {
+function WarehouseDetails({warehouseLocation, warehouseAddress, warehouseContact}) {
+  let contactName = warehouseContact.name;
+  let contactPosition = warehouseContact.position;
+  let contactPhone = warehouseContact.phone;
+  let contactEmail = warehouseContact.email;
+
   return (
-    <div>WarehouseDetails</div>
+    <>
+      <div className="warehouse-details-section">
+        <div className="warehouse-details-section__content">
+          <div className="warehouse-details-section__header">
+            <div className="warehouse-details-section__icon-and-text">
+              <img src={require('../../../assets/icons/arrow_back-24px.svg').default} className="warehouse-details-section__header-icon" alt="" />
+              <div className="add-item-section__title"> {warehouseLocation} </div>
+            </div>
+            <button className='warehouse-details-section__edit-button'>
+            <img src={require('../../../assets/icons/edit-24px.svg').default} className="warehouse-details-section__edit-icon" alt="" />
+            </button>
+          </div>
+          <div className="warehouse-details-section__contact-info">
+            <div className="warehouse-details-section__address-block">
+              <div className="warehouse-details-section__address-label">
+                WAREHOUSE ADDRESS:
+              </div>
+              <div className="warehouse-details-section__address-text">
+                {warehouseAddress}
+              </div>
+            </div>
+            <div className="warehouse-details-section__name-and-contact">
+              <div className="warehouse-details-section__name-block">
+                <div className="warehouse-details-section__name-label">
+                  CONTACT NAME:
+                </div>
+                <div className="warehouse-details-section__name-text">
+                   {contactName}
+                   <br />
+                  {contactPosition}
+                </div>
+              </div>
+              <div className="warehouse-details-section__info-block">
+                <div className="warehouse-details-section__info-label">
+                  CONTACT INFORMATION:
+                </div>
+                <div className="warehouse-details-section__info-text">
+                  {contactPhone} <br />
+                  {contactEmail}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/WarehousesPage.js
+++ src/pages/WarehousesPage.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import WarehouseDetails from '../components/Warehouse/DetailsWarehouse/WarehouseDetails'
 
 function WarehousesPage() {
+  
+  let textContactObj = {
+    name: "Alana Thomas",
+    position: "Warehouse Manager",
+    phone: "+1 (646) 123-1234",
+    email: "athomas@instock.com"
+  }
+  
   return (
+  <>
     <div>WarehousesPage</div>
+    <WarehouseDetails 
+    warehouseLocation="Washington"
+    warehouseAddress = "2650 NW 5th Avenue"
+    warehouseContact = {textContactObj}
+     />
+  </>
   )
 }
 
