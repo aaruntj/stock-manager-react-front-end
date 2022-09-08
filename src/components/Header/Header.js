b5c75cc48bec3a1395de07107a0ea170f3ea0113
Asif Iqbal <iqbal1@ualberta.ca>
2022-09-08 01:00:42 -0600

    added in fonts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,34 @@
-import LogoTitle from "../../assets/logo/InStock-Logo.svg"
+import LogoTitle from "../../assets/logo/InStock-Logo.svg";
 import "./Header.scss";
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-const Header= () =>{
-
-  return(
+const Header = () => {
+  return (
     <nav className="header">
-      <Link to="/" className="header__logo--link"> 
+      <Link to="/" className="header__logo--link">
         <section className="header__logo--container">
-          <img className="header__logo" src={LogoTitle} alt="instockLogoTitle"/>
+          <img
+            className="header__logo"
+            src={LogoTitle}
+            alt="instockLogoTitle"
+          />
         </section>
       </Link>
 
       <section className="header__contents--container">
         <section className="header__contents--left">
-          <NavLink to="/warehouses" className="header__contents"> 
-              Warehouses
+          <NavLink to="/warehouses" className="header__contents">
+            Warehouses
           </NavLink>
         </section>
         <section className="header__contents--right">
-          <NavLink to="/inventory" className="header__contents"> 
-              Inventory
+          <NavLink to="/inventory" className="header__contents">
+            Inventory
           </NavLink>
         </section>
       </section>
     </nav>
-  )
-}
+  );
+};
 
 export default Header;
