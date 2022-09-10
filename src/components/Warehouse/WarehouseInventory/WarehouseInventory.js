2e1456d2765879f0f8bd44b80d8c1c6f65a25ec6
Asif Iqbal <iqbal1@ualberta.ca>
2022-09-09 19:15:27 -0600

    fixed more styling

--- src/components/Warehouse/WarehouseInventory/WarehouseInventory.js
+++ src/components/Warehouse/WarehouseInventory/WarehouseInventory.js
@@ -7,37 +7,46 @@ import DeleteIcon from "../../../assets/icons/delete_outline-24px.svg";
 import EditIcon from "../../../assets/icons/edit-24px.svg";
 
 function Warehouses({warehouse}) {
+  const isStatusGreen = () =>{
+    if (warehouse.status.toUpperCase() === "OUT OF STOCK"){
+      return false
+    }
+    else return true
+  }
+  console.log(warehouse.status)
+  console.log(isStatusGreen(warehouse.status))
+
 	return (
-		<div className="card__section">
-			<div className="card">
-				<div className="card__container">
-					<div className="card__box">
-						<h4 className="card__label">INVENTORY ITEM</h4>
-						<div className="card__link-container">
-							<Link to={`/inventory/${warehouse.id}`} className="card__link">
-								<span className="card__item-link">{warehouse.itemName}</span>{" "}
+		<div className="warehouse__card__section">
+			<div className="warehouse__card">
+				<div className="warehouse__card__container">
+					<div className="warehouse__card__box">
+						<h4 className="warehouse__card__label">INVENTORY ITEM</h4>
+						<div className="warehouse__card__link-container">
+							<Link to={`/inventory/${warehouse.id}`} className="warehouse__card__link">
+								<span className="warehouse__card__item-link">{warehouse.itemName}</span>{" "}
 								<img src={ArrowIcon} alt="" />
 							</Link>
 						</div>
 					</div>
-					<div className="card__box">
-						<h4 className="card__label">CATEGOTY</h4>
-						<p className="card__item">{warehouse.category}</p>
+					<div className="warehouse__card__box">
+						<h4 className="warehouse__card__label">CATEGOTY</h4>
+						<p className="warehouse__card__item">{warehouse.category}</p>
 					</div>
 				</div>
 
-				<div className="card__container">
-					<div className="card__box">
-						<h4 className="card__label">STATUS</h4>
-						<p className="card__status-green">{warehouse.status.toUpperCase()}</p>
+				<div className="warehouse__card__container">
+					<div className="warehouse__card__box">
+						<h4 className="warehouse__card__label">STATUS</h4>
+						<p className={`${isStatusGreen(warehouse.status)?"warehouse__card__status-green": "warehouse__card__status-orange"}`}>{warehouse.status.toUpperCase()}</p>
 					</div>
-					<div className="card__box">
-						<h4 className="card__label">QTY</h4>
-						<p className="card__item">{warehouse.quantity}</p>
+					<div className="warehouse__card__box">
+						<h4 className="warehouse__card__label">QTY</h4>
+						<p className="warehouse__card__item">{warehouse.quantity}</p>
 					</div>
 				</div>
 			</div>
-			<div className="card__btn-container">
+			<div className="warehouse__card__btn-container">
 				<img src={DeleteIcon} alt="" />
 				<img src={EditIcon} alt="" />
 			</div>
