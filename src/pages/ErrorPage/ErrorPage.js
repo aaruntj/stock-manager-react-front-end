06718c49c5a61462c3bc7368785eed6dc31e0255
Aarun Jassal <aaruntj@gmail.com>
2022-09-12 13:53:59 -0700

    worked on error page component

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import "./ErrorPage.scss"
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
   return (
-    <div>This is the error page</div>
+    <>
+      <p className='error__text'>404 - Page not found</p>
+      <Link to="/warehouses">
+        <div className="error__btn">Back to Homepage</div>
+      </Link>
+    </>
   )
 }
 
